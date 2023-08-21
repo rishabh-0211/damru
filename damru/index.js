dc5a3fd412266817c355b0e831319e207f0a5949
Rishabh Sharma <errishabhsharma02@gmail.com>
2023-08-21 18:20:37 +0530

    added responsiveness to it

--- damru/index.js
+++ damru/index.js
@@ -13,31 +13,31 @@ document.addEventListener("keypress", function (event) {
 
 function makesound(x){
     switch (x) {
-        case "r":
+        case "s":
             var audio = new Audio('sounds/tom-1.mp3');
             audio.play();
             break;
-        case "i":
+        case "d":
             var audio = new Audio('sounds/tom-2.mp3');
             audio.play();
             break;
-        case "s":
+        case "f":
             var audio = new Audio('sounds/tom-3.mp3');
             audio.play();
             break;
-        case "h":
+        case "g":
             var audio = new Audio('sounds/tom-4.mp3');
             audio.play();
             break;
-        case "a":
+        case "h":
             var audio = new Audio('sounds/snare.mp3');
             audio.play();
             break;
-        case "b":
+        case "j":
             var audio = new Audio('sounds/crash.mp3');
             audio.play();
             break;
-        case "g":
+        case "k":
             var audio = new Audio('sounds/kick-bass.mp3');
             audio.play();
             break;
